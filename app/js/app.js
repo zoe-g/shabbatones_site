e19564965f346fee5c27521d1e706570eb57c327
Zoe <zoe.goldfarb@gmail.com>
2014-06-21 19:05:07 -0700

    music landing page

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,41 @@
 var app = angular.module('shabbatones', ['customFilters']);
 
+  app.directive('music', ['$http', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/music.html',
+      controller: function($scope, $http, $filter){
+        var music = this;
+        var spreadsheet_url = 'https://spreadsheets.google.com/feeds/list/140NStbyyUW95Kp5EjCQWjqh06kJNpF40aY-99gI5LMs/612845608/public/full?alt=json&orderby=releaseyear&reverse=true';
+        music.albums = [];
+
+        $http.get(spreadsheet_url).success(function(data){
+          music.albums = data.feed.entry;
+        });
+      },
+      controllerAs: 'music'
+    };
+  }]);
+
+  app.directive('album', ['$http', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/album.html',
+      controller: function($scope, $http, $filter){
+        var albums = this;
+        var spreadsheet_url = 'https://spreadsheets.google.com/feeds/list/140NStbyyUW95Kp5EjCQWjqh06kJNpF40aY-99gI5LMs/612845608/public/full?alt=json&orderby=releaseyear&reverse=true';
+        albums.cd = [];
+
+        $http.get(spreadsheet_url).success(function(data){
+          albums.cd = data.feed.entry;
+        });
+
+        cd = albums.cd[0]
+      },
+      controllerAs: 'album'
+    };
+  }]);
+
   app.directive('alumni', ['$http', function(){
     return {
       restrict: 'E',
@@ -22,3 +58,10 @@ var app = angular.module('shabbatones', ['customFilters']);
       controllerAs: 'alumni'
     };
   }]);
+
+  app.directive('about', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/about.html',
+    };
+  });
